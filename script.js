39d7c97ced35bdca6b1ea47e976f34da8c0bac0e
Anna Sürken <a.suerken@hotmail.de>
2025-06-14 14:54:44 +0200

    added html and render function for js

---
+++ script.js
@@ -0,0 +1,33 @@
+let fields = [null, null, null, null, null, null, null, null, null];
+
+
+function init() {
+    render();
+}
+
+function render() {
+  let html = "<table>";
+
+  for (let i = 0; i < 3; i++) {
+    html += "<tr>";
+    for (let j = 0; j < 3; j++) {
+      let index = i * 3 + j;
+      let symbol = "";
+
+      if (fields[index] === "circle") {
+        symbol = "O";
+      } else if (fields[index] === "cross") {
+        symbol = "X";
+      }
+
+      html += `<td>${symbol}</td>`;
+    }
+    html += "</tr>";
+  }
+
+  html += "</table>";
+
+  document.getElementById("content").innerHTML = html;
+}
+
+
